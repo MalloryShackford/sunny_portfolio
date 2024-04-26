acf3be75cb225a2161d32be614717b0e52a4ff03
Mallory Shackford <93612680+MalloryShackford@users.noreply.github.com>
2024-04-26 08:57:15 -0400

    form updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
@@ -5,6 +6,21 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import headshot from '../assets/headshot1.png';
 import '../styles/contact.css';
 const Contact = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({ name: '', email: '', message: '' });
+  }
+
   return (
     <>
     <div id="contact" className="contact-container">
@@ -17,33 +33,53 @@ const Contact = () => {
               <div className="form-submit-container">
                   <div className="form-container">
                   <form
-                    action="https://formsubmit.co/m.shackford@yahoo.com" method="POST">
-                    <input type="text" name="name" placeholder="Name" required/>
-                    <input type="email" name="email" placeholder="Email" required />
-                      <textarea style={{fontFamily: 'inter'}} placeholder="Your Message" className="textarea" name="message" rows="10" required></textarea>
-                        <button type="submit">Send</button>
+                    action="https://formsubmit.co/9eb8400d13809384a217392d8c97d16e" method="POST">
+                    <input 
+                      type="text" 
+                      name="name" 
+                      placeholder="Name" 
+                      value={formData.name}
+                      onChange={handleChange}
+                      required/>
+                    <input 
+                      type="email" 
+                      name="email" 
+                      placeholder="Email" 
+                      value={formData.email}
+                      onChange={handleChange}
+                      required />
+                      <textarea 
+                        style={{fontFamily: 'inter'}} 
+                        placeholder="Your Message" 
+                        className="textarea" 
+                        name="message" 
+                        value={formData.message}
+                        onChange={handleChange}
+                        rows="10" 
+                        required></textarea>
+                <button onChange={handleSubmit} type="submit">Send</button>
                   </form>
+                  </div>
               </div>
-         </div>
-      <div className="icon-container">
-      <div className='icon'>
-        <a href="https://www.linkedin.com/in/mshackford/" target="_blank" rel="noopener"><FaLinkedinIn style={{color: 'black'}}/></a>
-      </div>
-      <div className='icon'>
-          <a href="https://github.com/MalloryShackford" target="_blank" rel="noopener"><FaGithub style={{color: 'black'}} /></a>
-      </div>
-      <div className='icon'>
-          <a href="mailto:mallory.shackford@gmail.com"><AiOutlineMail style={{color: 'black'}} /></a>
-      </div>
-      <div className='icon'>
-         <a href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:8d02535f-319d-4470-b7aa-95522a206921" target="_blank" rel="noopener noreferrer" title="Resume"><BsFillPersonLinesFill style={{color: 'black'}} /></a>
-      </div>
-      </div>
-    </div>
-    <div>
-        <a href="/"><HiOutlineChevronDoubleUp className="icon chevron-up" size={30} />
+              <div className="icon-container">
+              <div className='icon'>
+                <a href="https://www.linkedin.com/in/mshackford/" target="_blank" rel="noopener"><FaLinkedinIn style={{color: 'black'}}/></a>
+              </div>
+              <div className='icon'>
+                  <a href="https://github.com/MalloryShackford" target="_blank" rel="noopener"><FaGithub style={{color: 'black'}} /></a>
+              </div>
+              <div className='icon'>
+                  <a href="mailto:mallory.shackford@gmail.com"><AiOutlineMail style={{color: 'black'}} /></a>
+              </div>
+              <div className='icon'>
+                 <a href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:8d02535f-319d-4470-b7aa-95522a206921" target="_blank" rel="noopener noreferrer" title="Resume"><BsFillPersonLinesFill style={{color: 'black'}} /></a>
+              </div>
+              </div>
+              </div>
+        <div>
+          <a href="/"><HiOutlineChevronDoubleUp className="icon chevron-up" size={30} />
           </a>
-    </div>
+        </div>
     </div>
     </>
   )
